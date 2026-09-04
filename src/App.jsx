4414d43a18485c9025a12d1ab8949e9552ed8e5d
Sadaf Kareem <sadafkareem234@gmail.com>
2026-09-04 17:12:28 +0500

    Add pages and update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import Profile from './Pages/Profile'
+import SignUp from './Pages/SignUP'
 
 export default function App(){
   return(
-  <div>
-    <h1 className='text-red-500'>Hello World</h1>
-  </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
 
   )
 
